923db18912b243cda4686e7e36babb8b99a52c37
anshika8286 <anshikapundir773@gmail.com>
2025-10-20 19:01:36 +0530

    Q39.c

---
+++ DAY20/Q39.c
@@ -0,0 +1,20 @@
+//Q39: Write a program to find the product of odd digits of a number.
+#include <stdio.h>
+int main() {
+    int n, digit;
+    int product = 1;
+    int hasOdd = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(n > 0) {
+        digit = n % 10;      
+        if(digit % 2 != 0) { 
+            product *= digit;
+            hasOdd = 1;
+        }
+        n = n / 10;      
+    }
+    printf("Product of odd digits = %d\n", product);
+    return 0;
+}
+
