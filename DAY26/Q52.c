cea868d85622895ad6b88860e40c2f4656ba6147
anshika8286 <anshikapundir773@gmail.com>
2025-10-24 16:30:02 +0530

    Q52.c

---
+++ DAY26/Q52.c
@@ -0,0 +1,49 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*#include <stdio.h>
+
+int main() {
+    // First single star
+    printf("*\n\n");
+
+    // Two stars
+    printf("*\n*\n\n");
+
+    // Five stars
+    printf("*\n*\n*\n*\n*\n\n");
+
+    // Three stars
+    printf("*\n*\n*\n\n");
+
+    // One star
+    printf("*\n");
+
+    return 0;
+}
+
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+**/
+#include <stdio.h>
+int main() {
+ printf("*\n\n");
+    printf("*\n*\n\n");
+    printf("*\n*\n*\n*\n*\n\n");
+    printf("*\n*\n*\n\n");
+    printf("*\n");
+
+    return 0;
+}
