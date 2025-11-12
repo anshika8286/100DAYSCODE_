cab5f926076e1019c63334c9b2f977ce2f79e1ad
anshika8286 <anshikapundir773@gmail.com>
2025-11-12 19:59:43 +0530

    Q92.c

---
+++ DAY46/Q92.c
@@ -0,0 +1,19 @@
+//Q92: Find the first repeating lowercase alphabet in a string.
+#include <stdio.h>
+int main() {
+    char str[100], result[100];
+    int i, j = 0;
+    scanf("%s", str);
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' &&
+            str[i] != 'A' && str[i] != 'E' && str[i] != 'I' &&
+            str[i] != 'O' && str[i] != 'U') {
+            result[j++] = str[i];
+        }
+    }
+
+    result[j] = '\0'; 
+    printf("%s", result);
+    return 0;
+}
