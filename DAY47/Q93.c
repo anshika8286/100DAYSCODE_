b666a5cef9c3627fae57cd5d1e155b1813a57745
anshika8286 <anshikapundir773@gmail.com>
2025-11-16 12:40:32 +0530

    Q93.c

---
+++ DAY47/Q93.c
@@ -0,0 +1,26 @@
+//Q93: Check if two strings are anagrams of each other.
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str1[100], str2[100];
+    int freq1[256] = {0}, freq2[256] = {0};
+    int i;
+    scanf("%s", str1);
+    scanf("%s", str2);
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not anagrams");
+        return 0;
+    }
+    for (i = 0; str1[i] != '\0'; i++) {
+        freq1[(unsigned char)str1[i]]++;
+        freq2[(unsigned char)str2[i]]++;
+    }
+    for (i = 0; i < 256; i++) {
+        if (freq1[i] != freq2[i]) {
+            printf("Not anagrams");
+            return 0;
+        }
+    }
+    printf("Anagrams");
+    return 0;
+}
