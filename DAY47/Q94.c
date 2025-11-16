63f45faec28b39a4caace73fefa258d9ec0db6b6
anshika8286 <anshikapundir773@gmail.com>
2025-11-16 12:44:33 +0530

    Q94.c

---
+++ DAY47/Q94.c
@@ -0,0 +1,23 @@
+//Q94: Find the longest word in a sentence.
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[200], word[50], longest[50] = "";
+    int i = 0, j = 0;
+    fgets(str, sizeof(str), stdin);
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && str[i] != '\n') {
+            word[j++] = str[i];
+        } 
+        else {
+            word[j] = '\0';  
+            if (strlen(word) > strlen(longest)) {
+                strcpy(longest, word);
+            }
+            j = 0; 
+        }
+        i++;
+    }
+    printf("%s", longest);
+    return 0;
+}
